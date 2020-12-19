51b7e71b6b3a63b5b06624556f0e26df6213c267
Anna <anna.sidorova.feri@gmail.com>
2020-12-19 22:09:59 +0100

    Added messaging

---
+++ app/src/main/java/edu/um/feri/pora/foodtinder/ConversationsActivity.java
@@ -0,0 +1,13 @@
+package edu.um.feri.pora.foodtinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConversationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversations);
+    }
+}
